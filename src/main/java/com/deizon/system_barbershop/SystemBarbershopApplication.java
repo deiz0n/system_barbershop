d02758489e1784081543f99bb29683d2b932091e
deiz0n <eduardomacedosilva@outlook.com>
2023-10-06 08:55:23 -0300

    Criação do projeto

---
+++ src/main/java/com/deizon/system_barbershop/SystemBarbershopApplication.java
@@ -0,0 +1,13 @@
+package com.deizon.system_barbershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBarbershopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemBarbershopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deizon/system_barbershop/SystemBarbershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deizon.system_barbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemBarbershopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
