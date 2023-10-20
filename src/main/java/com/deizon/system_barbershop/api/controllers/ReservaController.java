619f1c851d36b3cba073d894b2046625e5653c91
deiz0n <eduardomacedosilva@outlook.com>
2023-10-20 16:55:36 -0300

    Criação do ReservaController

---
+++ src/main/java/com/deizon/system_barbershop/api/controllers/ReservaController.java
@@ -0,0 +1,55 @@
+package com.deizon.system_barbershop.api.controllers;
+
+import com.deizon.system_barbershop.domain.dtos.ReservaDTO;
+import com.deizon.system_barbershop.domain.services.ReservaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/reservas")
+public class ReservaController {
+
+    private ReservaService reservaService;
+
+    @Autowired
+    public ReservaController(ReservaService reservaService) {
+        this.reservaService = reservaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReservaDTO>> getReservas() {
+        var reservas = reservaService.findAll();
+        return ResponseEntity.ok().body(reservas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReservaDTO> getReserva(@PathVariable UUID id) {
+        var reserva = reservaService.findByID(id);
+        return ResponseEntity.ok().body(reserva);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createReserva(@RequestBody @PathVariable ReservaDTO newReserva) {
+        var reserva = reservaService.addResource(newReserva);
+        return ResponseEntity.status(HttpStatus.CREATED).body(reserva);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteReserva(@PathVariable UUID id) {
+        reservaService.remResource(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateReserva(@PathVariable UUID id, @RequestBody @Valid ReservaDTO newReserva) {
+        var reserva = reservaService.updateResource(id, newReserva);
+        return ResponseEntity.ok().body(reserva);
+    }
+
+}
---
+++ src/main/java/com/deizon/system_barbershop/domain/services/ReservaService.java
@@ -0,0 +1,79 @@
+package com.deizon.system_barbershop.domain.services;
+
+import com.deizon.system_barbershop.domain.dtos.ReservaDTO;
+import com.deizon.system_barbershop.domain.models.Reserva;
+import com.deizon.system_barbershop.domain.repositories.ReservaRepository;
+import com.deizon.system_barbershop.domain.services.DTOMapper.ReservaDTOMapper;
+import com.deizon.system_barbershop.domain.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class ReservaService implements ServiceCRUD<ReservaDTO, Reserva>{
+
+    private ReservaRepository reservaRepository;
+
+    private ReservaDTOMapper reservaDTOMapper;
+
+    @Autowired
+    public ReservaService(ReservaRepository reservaRepository, ReservaDTOMapper reservaDTOMapper) {
+        this.reservaRepository = reservaRepository;
+        this.reservaDTOMapper = reservaDTOMapper;
+    }
+
+    @Override
+    public List<ReservaDTO> findAll() {
+        var reservas = reservaRepository.findAll()
+                .stream()
+                .map(reservaDTOMapper)
+                .collect(Collectors.toList());
+        return reservas;
+    }
+
+    @Override
+    public ReservaDTO findByID(UUID id) {
+        var reserva = reservaRepository.findById(id)
+                .map(reservaDTOMapper);
+        return reserva.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    @Override
+    public Reserva addResource(ReservaDTO reservaDTO) {
+        var reserva = new Reserva();
+        BeanUtils.copyProperties(reservaDTO, reserva);
+        return reservaRepository.save(reserva);
+    }
+
+    @Override
+    public void remResource(UUID id) {
+        var reserva = reservaRepository.findById(id);
+        if (reserva.isPresent()) {
+            reservaRepository.delete(reserva.get());
+        } else {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    @Override
+    public Reserva updateResource(UUID id, ReservaDTO newReserva) {
+        var reserva = reservaRepository.getReferenceById(id);
+        try {
+            updateDataResource(reserva, newReserva);
+            return reservaRepository.save(reserva);
+        } catch (EntityNotFoundException error) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    @Override
+    public void updateDataResource(Reserva oldResource, ReservaDTO newResource) {
+        oldResource.setCliente(newResource.getCliente());
+        oldResource.setHorario(newResource.getHorario());
+    }
+}
