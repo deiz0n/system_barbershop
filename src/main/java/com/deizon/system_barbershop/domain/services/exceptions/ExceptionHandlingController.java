ead542a6a797bf3ec994661c95ff306348bedfa8
deiz0n <eduardomacedosilva@outlook.com>
2024-01-01 22:44:59 -0300

    Correções ao retornar a mensagem de erro e uri.

--- src/main/java/com/deizon/system_barbershop/domain/services/exceptions/ExceptionHandlingController.java
+++ src/main/java/com/deizon/system_barbershop/domain/services/exceptions/ExceptionHandlingController.java
@@ -7,7 +7,6 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.time.Instant;
@@ -16,65 +15,65 @@ import java.time.Instant;
 public class ExceptionHandlingController {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> resourceNotFound(ResourceNotFoundException exception, WebRequest request) {
+    public ResponseEntity<?> resourceNotFound(ResourceNotFoundException exception, HttpServletRequest request) {
         var error = new Error(
                 Instant.now(),
                 HttpStatus.NOT_FOUND,
                 exception.getMessage(),
-                request.getDescription(false)
+                request.getRequestURI()
         );
         return ResponseEntity.status(404).body(error);
     }
 
     @ExceptionHandler(ExistingFieldException.class)
-    public ResponseEntity<?> existingField(ExistingFieldException exception, WebRequest request) {
+    public ResponseEntity<?> existingField(ExistingFieldException exception, HttpServletRequest request) {
         var error = new Error(
                 Instant.now(),
                 HttpStatus.CONFLICT,
-                exception.getMessage().toUpperCase(),
-                request.getDescription(false)
+                exception.getMessage(),
+                request.getRequestURI()
         );
         return ResponseEntity.status(409).body(error);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> argumentInvalid(MethodArgumentNotValidException exception, WebRequest request) {
+    public ResponseEntity<?> argumentInvalid(MethodArgumentNotValidException exception, HttpServletRequest request) {
         var stringBuilder = new StringBuilder();
         stringBuilder.append(exception.getFieldError().getField().toUpperCase());
-        stringBuilder.append(" INVÁLIDO");
+        stringBuilder.append(" inválido.");
         var error = new Error(
                 Instant.now(),
                 HttpStatus.BAD_REQUEST,
                 stringBuilder.toString(),
-                request.getDescription(false)
+                request.getRequestURI()
         );
         return ResponseEntity.status(400).body(error);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<?> dateInvalid(WebRequest request) {
+    public ResponseEntity<?> dateInvalid(HttpServletRequest request) {
         var error = new Error(
                 Instant.now(),
                 HttpStatus.BAD_REQUEST,
-                "O HORÁRIO INFORMADO POSSUI FORMATO INVÁLIDO",
-                request.getDescription(false)
+                "O horário informado possui formato inválido.",
+                request.getRequestURI()
         );
         return ResponseEntity.status(400).body(error);
     }
 
     @ExceptionHandler(ArgumentNotValidException.class)
-    public ResponseEntity<?> dateShort(ArgumentNotValidException exception, WebRequest request) {
+    public ResponseEntity<?> dateShort(ArgumentNotValidException exception, HttpServletRequest request) {
         var error = new Error(
                 Instant.now(),
                 HttpStatus.BAD_REQUEST,
-                exception.getMessage().toUpperCase(),
-                request.getDescription(false)
+                exception.getMessage(),
+                request.getRequestURI()
         );
         return ResponseEntity.status(400).body(error);
     }
 
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public ResponseEntity<?> contraintVioletion(SQLIntegrityConstraintViolationException exception, WebRequest request) {
+    public ResponseEntity<?> contraintVioletion(SQLIntegrityConstraintViolationException exception, HttpServletRequest request) {
         var msg = new String();
         if (exception.getMessage().contains("FK40agr0nhu8t21hlb0s4bifbsp")) {
             msg = "O horário informado não foi encontrado.";
@@ -86,7 +85,7 @@ public class ExceptionHandlingController {
                 Instant.now(),
                 HttpStatus.BAD_REQUEST,
                 msg,
-                request.getDescription(false)
+                request.getRequestURI()
         );
     return ResponseEntity.status(400).body(error);
     }
